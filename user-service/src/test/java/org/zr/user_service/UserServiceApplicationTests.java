445eb1b422c03d47ae24d11c50f2d23b02ef4263
ZRishu <ZRishu@hotmail.com>
2026-02-05 22:31:13 +0530

    feat(user): add test to create users

--- user-service/src/test/java/org/zr/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/org/zr/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package org.zr.user_service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zr.user_service.domain.entity.User;
+import org.zr.user_service.repository.UserRepository;
 
+@Slf4j
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static final int NUMBER_OF_USERS = 10;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Disabled
+    @Test
+    void createUser() {
+        for (int i = 1; i <= NUMBER_OF_USERS; i++) {
+            User user = User.builder()
+                    .name("user" + i)
+                    .surname("surname" + i)
+                    .email("user" + i + "example.com")
+                    .address(i + "Example street")
+                    .alerting(i % 2 == 0)
+                    .energyAlertingThreshold(1000.0 + i)
+                    .build();
+            userRepository.save(user);
+        }
+        log.info("user repository populated successfully");
+    }
 
 }

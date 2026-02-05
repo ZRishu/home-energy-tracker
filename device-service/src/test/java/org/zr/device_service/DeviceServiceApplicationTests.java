c250a1ab20d6dfa6bc6c7144d47b787d7fd7fd5c
ZRishu <ZRishu@hotmail.com>
2026-02-05 22:31:26 +0530

    feat(device): add test to create devices

--- device-service/src/test/java/org/zr/device_service/DeviceServiceApplicationTests.java
+++ device-service/src/test/java/org/zr/device_service/DeviceServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package org.zr.device_service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zr.device_service.domain.entity.Device;
+import org.zr.device_service.model.DeviceType;
+import org.zr.device_service.repository.DeviceRepository;
 
+@Slf4j
 @SpringBootTest
 class DeviceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static final int NUMBER_OF_DEVICES = 200;
+    public static final int USERS = 10;
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Disabled
+    @Test
+    void createDevice() {
+        for (int i = 1; i <= NUMBER_OF_DEVICES; i++) {
+            Device device = Device.builder()
+                    .name("device" + i)
+                    .type(DeviceType.values()[i % DeviceType.values().length])
+                    .location("location" + ((i % 3) + 1))
+                    .userId((long) ((i % USERS) + 1))
+                    .build();
+            deviceRepository.save(device);
+        }
+        log.info("Device repository has been populated");
+    }
 
 }

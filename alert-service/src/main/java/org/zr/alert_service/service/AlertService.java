d54dfb298e5f3868be91db006878770dae90763f
ZRishu <ZRishu@hotmail.com>
2026-02-19 01:03:49 +0530

    feat(alert): send email of each alert from kafka topic

--- alert-service/src/main/java/org/zr/alert_service/service/AlertService.java
+++ alert-service/src/main/java/org/zr/alert_service/service/AlertService.java
@@ -9,9 +9,22 @@ import org.zr.kafka.event.AlertingEvent;
 @Service
 public class AlertService {
 
+    private final EmailService emailService;
+
+    public AlertService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @KafkaListener(topics = "energy-alerts", groupId = "alert-service")
     public void energyUsageAlertEvent(AlertingEvent alertingEvent) {
         log.info("Recieved alerting event: {}", alertingEvent);
-        
+
+        // Send email alert
+        final String subject = "Energy usage alert for User: " + alertingEvent.getUserId();
+        final String message = "Alert: " + alertingEvent.getMessage() +
+                "\nThreshold: " + alertingEvent.getThreshold() +
+                "\nEnergy consumed: " + alertingEvent.getEnergyConsumed();
+
+        emailService.sendEmail(alertingEvent.getEmail(), subject, message, alertingEvent.getUserId());
     }
 }

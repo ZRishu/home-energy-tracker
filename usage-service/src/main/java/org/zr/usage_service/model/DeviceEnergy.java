56853a0d85b0f85c6af316bc58a879c81370cf8f
ZRishu <ZRishu@hotmail.com>
2026-02-18 16:30:09 +0530

    fix(usage): change device energy record into pojo to make it mutable

--- usage-service/src/main/java/org/zr/usage_service/model/DeviceEnergy.java
+++ usage-service/src/main/java/org/zr/usage_service/model/DeviceEnergy.java
@@ -1,10 +1,16 @@
 package org.zr.usage_service.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
-public record DeviceEnergy(
-        Long deviceId,
-        double energyConsumed,
-        Long userId) {
+public class DeviceEnergy {
+    Long deviceId;
+    double energyConsumed;
+    Long userId;
 }
